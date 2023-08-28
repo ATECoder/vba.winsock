ff3b41e528cf9422311da95fe9c0d048b8ec7923
David Hary <791370+ATECoder@users.noreply.github.com>
2023-08-28 12:59:06 -0700

    add power shell scripts for opening the workbooks

---
+++ src/winsock/open.workbook.ps1
@@ -0,0 +1,86 @@
+# ----------------------------------------------------------------------
+# Localize
+#
+# PURPOSE: open and save all workbooks from the bin folder thus localizing their references.
+#
+# CALLING SCRIPT:
+#
+#  ."open.workbook.ps1"
+#
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# VARIABLES
+
+$CWD = (Resolve-Path .\).Path
+$Bdir = $CWD
+$Bdir = (Resolve-Path $Bdir).Path
+$XL_FILE_FORMAT_MACRO_ENABLED = 52
+
+# END VARIABLES
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# FUNCTIONS
+
+Function LogInfo($message)
+{
+    Write-Host $message -ForegroundColor Gray
+}
+
+Function LogError($message)
+{
+    Write-Host $message -ForegroundColor Red
+}
+
+Function LogEmptyLine()
+{
+    echo ""
+}
+
+# END FUNCTIONS
+# ----------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------
+# SCRIPT ENTRY POINT
+
+$DEBUG = $true
+
+# declare Excel
+$excel = New-Object -ComObject Excel.Application
+$excel.DisplayAlerts = $false;
+$excel.EnableEvents = $false;
+
+$missing = [System.Reflection.Missing]::Value
+
+$UpdateLinks = $missing
+$ReadOnly = $true
+$Format = $missing
+$Password = $missing
+$WriteReservedPassword = $missing
+$IgnoreReadOnlyDisplay = $true
+
+$src = "C:\my\lib\vba\core\core\src\io\cc.isr.core.io.xlsm"
+LogInfo( "opening " + $src)
+$book = $excel.Workbooks.Open($src, $missing, $true, $missing, $missing, $missing, $true)
+LogInfo ( "Opened " + $excel.ActiveWorkbook.Name )
+
+$src = "C:\my\lib\vba\core\core\src\core\cc.isr.core.xlsm"
+LogInfo( "opening " + $src)
+$book = $excel.Workbooks.Open($src, $missing, $true, $missing, $missing, $missing, $true)
+LogInfo ( "Opened " + $excel.ActiveWorkbook.Name )
+
+$excel.EnableEvents = $true;
+
+$src = "C:\my\lib\vba\iot\winsock\src\winsock\cc.isr.winsock.xlsm"
+LogInfo( "opening " + $src)
+$book = $excel.Workbooks.Open($src, $missing, $false, $missing, $missing, $missing, $true)
+LogInfo ( "Opened " + $excel.ActiveWorkbook.Name )
+
+LogInfo( "project loaded. Script will close in 5 seconds" )
+Start-Sleep -Seconds 5
+# $z = Read-Host "Press enter to exit"
+
+exit 0
+
